c753a9cd437fd5184d9505f8c7e83afc635b0ac1
Thomas Rootwelt <grasulf@hotmail.com>
2026-02-17 11:22:26 +0100

    Spawner working

--- fish/spawner.gd
+++ fish/spawner.gd
@@ -1,14 +1,27 @@
 extends Node2D
 
 @export var fish_scenes : Array[PackedScene]
+var number_of_fish = 4
 
 func _process(delta: float) -> void:
-	var current_depth = global_position.y
+	pass
 	
 
 func spawn_fish():
-	pass
-
+	var fish = fish_scenes.pick_random().instantiate()
+	
+	var spawn_pos = Vector2(global_position.x + randf_range(-1000, 1000), global_position.y + randf_range(1000, 2000))
+	
+	var spawn_right = spawn_pos[0] < global_position.x
+	
+	if not spawn_right:
+		fish.scale.x = -fish.scale.x
+	
+	fish.global_position = spawn_pos
+	
+	#Surely this is clean!
+	get_parent().get_parent().add_child(fish)
 
 func _on_timer_timeout() -> void:
-	spawn_fish() # Replace with function body.
+	for i in range(number_of_fish):
+		spawn_fish() # Replace with function body.
